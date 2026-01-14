26fc636e6e9c01ffc5183e4fac542524f92862de
Omar Rodriguez <omarr25lakers@gmail.com>
2026-01-13 20:52:04 -0700

    Testing

---
+++ bio.py
@@ -0,0 +1,11 @@
+
+def main():
+    print("My home is where my heart is!");
+    print("In my free time I like to play volleyball and go to the gym.");
+    print("I hope to make a good living off of my degree and make a business");
+    print("I have written in JS, TS, Python, C++, Swift, C, RISC V. My favorite has to be C++");
+    print("Something unique about me is I play guitar sometimes");
+
+
+if __name__ == "__main__":
+    main()
